86e50f57eac3d3a66afb47030984f2bab175f84a
Ashish Singh <sing0794@gmail.com>
2012-02-12 18:06:34 -0600

    NonSplitTextInputFormat

---
+++ src/convolution/rchannel/NonSplittableTextInputFormat.java
@@ -0,0 +1,11 @@
+package convolution.rchannel;
+
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapred.TextInputFormat;
+
+public class NonSplittableTextInputFormat extends TextInputFormat {
+ @Override
+ protected boolean isSplitable(FileSystem fs, Path file) {
+   return false;
+ }
+}
