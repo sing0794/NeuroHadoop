560e09fc27e03470b62f4cec9fb178661441eb00
Ashish Singh <sing0794@gmail.com>
2012-02-26 15:40:24 -0600

    script refresh input

--- nh.sh
+++ nh.sh
@@ -1,7 +1,10 @@
 ﻿#!/bin/bash
 
 hadoop fs -rmr output
+hadoop fs -rmr input
+hadoop fs -rmr lookup
 hadoop fs -mkdir input
+hadoop fs -mkdir lookup
 hadoop fs -put ~/data/morlet-2000.csv lookup/morlet-2000.dat
 hadoop fs -put ~/data/signals/*.csv input/
 hadoop jar NeuroHadoop-0.1.0.jar convolution.rchannel.ConvolutionJob input output > output.txt
