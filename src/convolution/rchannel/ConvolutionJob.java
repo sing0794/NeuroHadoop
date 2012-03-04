12d5e9e05d12b3e8b91b16781898fff4bb085031
Ashish Singh <sing0794@gmail.com>
2012-03-04 14:34:20 -0600

    Output structure based on input filename

--- src/convolution/rchannel/ConvolutionJob.java
+++ src/convolution/rchannel/ConvolutionJob.java
@@ -1,6 +1,7 @@
 package convolution.rchannel;
 
 import java.io.IOException;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +16,7 @@ import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.TextOutputFormat;
-import org.apache.hadoop.mapred.SequenceFileOutputFormat;
+import org.apache.hadoop.mapred.lib.MultipleSequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -31,71 +31,94 @@ import org.apache.hadoop.util.ToolRunner;
  */
 
 public class ConvolutionJob extends Configured implements Tool {
-    public static final String HDFS_KERNEL = "lookup/morlet-2000.dat";
+	static final String HDFS_KERNEL = "lookup/morlet-2000.dat";
 
-    public void cacheKernel(JobConf conf) throws IOException {
-        FileSystem fs = FileSystem.get(conf);
-        Path hdfsPath = new Path(HDFS_KERNEL);
+	static void cacheKernel(JobConf conf) throws IOException {
+		FileSystem fs = FileSystem.get(conf);
+		Path hdfsPath = new Path(HDFS_KERNEL);
+		DistributedCache.addCacheFile(hdfsPath.toUri(), conf);
+	}
 
-        DistributedCache.addCacheFile(hdfsPath.toUri(), conf);
-    }
+	static class MultiFileOutput extends MultipleSequenceFileOutputFormat<Text, Text> {
+		protected String getInputFileBasedOutputFileName(JobConf conf, String name) {
+			String fpath = conf.get("map.input.file");
+			String fname = new File(fpath).getName();
+
+			String ratnumber;
+			String sessiondate;
+			String channelid;
+			
+			int indexBegin = 0;
+			int indexEnd = fname.indexOf('-');
+
+			ratnumber = fname.substring(indexBegin, indexEnd);
+			indexBegin = indexEnd+1;
+			indexEnd = fname.indexOf('-', indexBegin);
+			sessiondate = fname.substring(indexBegin, indexEnd);
+			indexBegin = indexEnd+1;
+			indexEnd = fname.indexOf('-', indexBegin);
+			sessiondate = sessiondate + '-' + fname.substring(indexBegin, indexEnd);
+			indexBegin = indexEnd+1;
+			indexEnd = fname.indexOf('-', indexBegin);
+			sessiondate = sessiondate + '-' + fname.substring(indexBegin, indexEnd);
+			indexBegin = indexEnd+4;
+			indexEnd = fname.indexOf('.', indexBegin);
+			channelid = fname.substring(indexBegin, indexEnd);
+
+			return "rat=" + ratnumber +
+				"/dt=" + sessiondate +
+				"/channel=" + channelid + "/" +
+				ratnumber + "-" + sessiondate + "-" + channelid
+			;
+		}
+	}
 
 	@Override
 	public int run(String[] args) throws Exception {
 
 		System.out.println("\n\nConvolutionJob\n");
-
 		JobConf conf = new JobConf(getConf(), ConvolutionJob.class);
 		conf.setJobName("ConvolutionJob");
 		
-        this.cacheKernel(conf);
-
+		this.cacheKernel(conf);
 		conf.setMapperClass(ConvolutionMapper.class);
+		List<String> other_args = new ArrayList<String>();
+		for(int i=0; i < args.length; ++i) {
+			try {
+				if ("-m".equals(args[i])) {
+					conf.setNumMapTasks(Integer.parseInt(args[++i]));
+				} else if ("-r".equals(args[i])) {
+					conf.setNumReduceTasks(Integer.parseInt(args[++i]));
+				} else {
+					other_args.add(args[i]);
+				}
+			} catch (NumberFormatException except) {
+				System.out.println("ERROR: Integer expected instead of " + args[i]);
+				return printUsage();
+			} catch (ArrayIndexOutOfBoundsException except) {
+				System.out.println("ERROR: Required parameter missing from " + args[i-1]);
+				return printUsage();
+			}
+		}
+		
+		// Make sure there are exactly 2 parameters left.
+		if (other_args.size() != 2) {
+			System.out.println("ERROR: Wrong number of parameters: " + other_args.size() + " instead of 2.");
+			return printUsage();
+		}
 
-        List<String> other_args = new ArrayList<String>();
-        for(int i=0; i < args.length; ++i) {
-         try {
-           if ("-m".equals(args[i])) {
-           	
-             conf.setNumMapTasks(Integer.parseInt(args[++i]));
-             
-           } else if ("-r".equals(args[i])) {
-           	
-             conf.setNumReduceTasks(Integer.parseInt(args[++i]));
-	           		    	   
-           } else {
-           	
-             other_args.add(args[i]);
-             
-           }
-         } catch (NumberFormatException except) {
-           System.out.println("ERROR: Integer expected instead of " + args[i]);
-           return printUsage();
-         } catch (ArrayIndexOutOfBoundsException except) {
-           System.out.println("ERROR: Required parameter missing from " +
-                              args[i-1]);
-           return printUsage();
-         }
-        }
-        // Make sure there are exactly 2 parameters left.
-        if (other_args.size() != 2) {
-         System.out.println("ERROR: Wrong number of parameters: " +
-                            other_args.size() + " instead of 2.");
-         return printUsage();
-        }
-	
 		conf.setNumReduceTasks(0);
 		conf.setInputFormat(NonSplittableTextInputFormat.class);
-		conf.setOutputFormat(SequenceFileOutputFormat.class);
+		conf.setOutputFormat(MultiFileOutput.class);
 		conf.setOutputKeyClass(NullWritable.class);
 		conf.setOutputValueClass(Text.class);
 		conf.setCompressMapOutput(true);
-    	conf.set("mapred.output.compression.codec", "org.apache.hadoop.io.compress.SnappyCodec");
+		conf.set("mapred.output.compression.codec", "org.apache.hadoop.io.compress.SnappyCodec");
 		conf.set("mapred.output.compression.type", "BLOCK");
 
 		FileInputFormat.setInputPaths(conf, other_args.get(0));
 		FileOutputFormat.setOutputPath(conf, new Path(other_args.get(1)));
-		FileOutputFormat.setCompressOutput(conf, true);
+		// FileOutputFormat.setCompressOutput(conf, true);
 
 		JobClient.runJob(conf);
 
@@ -109,7 +132,6 @@ public class ConvolutionJob extends Configured implements Tool {
 	}
 
 	public static void main(String[] args) throws Exception {
-
 		int res = ToolRunner.run(new Configuration(), new ConvolutionJob(), args);
 		System.exit(res);
 
--- src/convolution/rchannel/ConvolutionMapper.java
+++ src/convolution/rchannel/ConvolutionMapper.java
@@ -3,7 +3,6 @@ package convolution.rchannel;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.File;
 import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
@@ -22,17 +21,16 @@ import org.apache.log4j.Logger;
 /**
  * ConvolutionMapper
  * 
- * 
  */
 
 public class ConvolutionMapper extends MapReduceBase implements
 		Mapper<LongWritable, Text, NullWritable, Text> {
 
-    public static final String HDFS_KERNEL = "lookup/morlet-2000.dat";
-    public static final int SIGNAL_BUFFER_SIZE = 10000000;
-    public static final int KERNEL_START_FREQ = 5;
-    public static final int KERNEL_END_FREQ = 200;
-    public static final int KERNEL_WINDOW_SIZE = 2001;
+	public static final String HDFS_KERNEL = "lookup/morlet-2000.dat";
+	public static final int SIGNAL_BUFFER_SIZE = 10000000;
+	public static final int KERNEL_START_FREQ = 5;
+	public static final int KERNEL_END_FREQ = 200;
+	public static final int KERNEL_WINDOW_SIZE = 2001;
 
 	static enum Parse_Counters {
 		BAD_PARSE
@@ -40,144 +38,124 @@ public class ConvolutionMapper extends MapReduceBase implements
 
 	private JobConf configuration;
 
-    private String ratnumber;
-    private String sessiondate;
-    private String channelid;
-
 	private final Text out_value = new Text();
 
 	private final TimeseriesDataPoint val = new TimeseriesDataPoint();
-    private HashMap<Integer, String> kernelMap;
-    private short[][] kernelStack = new short[KERNEL_END_FREQ+1][KERNEL_WINDOW_SIZE];
-    
-    private long[] ckConvolution = new long[KERNEL_END_FREQ+1];
-    private short[] signal = new short[SIGNAL_BUFFER_SIZE];
-    private int n = 0;
+	private HashMap<Integer, String> kernelMap;
+	private short[][] kernelStack = new short[KERNEL_END_FREQ+1][KERNEL_WINDOW_SIZE];
+	
+	private long[] ckConvolution = new long[KERNEL_END_FREQ+1];
+	private short[] signal = new short[SIGNAL_BUFFER_SIZE];
+	private int n = 0;
 
-    private RChannelDataPoint rec;
-    
-    private long lastTimestamp = 0;
+	private RChannelDataPoint rec;
+	
+	private long lastTimestamp = 0;
 
 	private static final Logger logger = Logger
 			.getLogger(ConvolutionMapper.class);
 
-    @Override
+	@Override
 	public void configure(JobConf conf) {
 		this.configuration = conf;
-        String fpath = conf.get("map.input.file");
-        String fname = new File(fpath).getName();
-        
-        int indexBegin = 0;
-        int indexEnd = fname.indexOf('-');
-        ratnumber = fname.substring(indexBegin, indexEnd);
-        indexBegin = indexEnd+1;
-        indexEnd = fname.indexOf('-', indexBegin);
-        sessiondate = fname.substring(indexBegin, indexEnd);
-        indexBegin = indexEnd+1;
-        indexEnd = fname.indexOf('-', indexBegin);
-        sessiondate = sessiondate + '-' + fname.substring(indexBegin, indexEnd);
-        indexBegin = indexEnd+1;
-        indexEnd = fname.indexOf('-', indexBegin);
-        sessiondate = sessiondate + '-' + fname.substring(indexBegin, indexEnd);
-        indexBegin = indexEnd+4;
-        indexEnd = fname.indexOf('.', indexBegin);
-        channelid = fname.substring(indexBegin, indexEnd);
-        
-        try {
-            String kernelCacheName = new Path(HDFS_KERNEL).getName();
-            Path [] cacheFiles = DistributedCache.getLocalCacheFiles(conf);
-            if (null != cacheFiles && cacheFiles.length > 0) {
-                for (Path cachePath : cacheFiles) {
-                    if (cachePath.getName().equals(kernelCacheName)) {
-                        loadKernel(cachePath);
-                        break;
-                    }
-                }
-                for (int i=KERNEL_START_FREQ; i <=   KERNEL_END_FREQ; i++) {
-                    kernelStack[i] = ConvertStringArrayToShortArray(kernelMap.get(i).split(","));
-                }
-            }
-        } catch (IOException ioe) {
-            System.err.println("IOException reading from distributed cache");
-            System.err.println(ioe.toString());
-        }
+		
+		try {
+			String kernelCacheName = new Path(HDFS_KERNEL).getName();
+			Path [] cacheFiles = DistributedCache.getLocalCacheFiles(conf);
+			if (null != cacheFiles && cacheFiles.length > 0) {
+				for (Path cachePath : cacheFiles) {
+					if (cachePath.getName().equals(kernelCacheName)) {
+						loadKernel(cachePath);
+						break;
+					}
+				}
+				for (int i=KERNEL_START_FREQ; i <=   KERNEL_END_FREQ; i++) {
+					kernelStack[i] = ConvertStringArrayToShortArray(kernelMap.get(i).split(","));
+				}
+			}
+		} catch (IOException ioe) {
+			System.err.println("IOException reading from distributed cache");
+			System.err.println(ioe.toString());
+		}
 	}
 
-    public void loadKernel(Path cachePath) throws IOException {
-        BufferedReader kernelReader = new BufferedReader(new FileReader(cachePath.toString()));
-        try {
-            String line = "";
-            int kernelFreq = KERNEL_START_FREQ;
-            this.kernelMap = new HashMap<Integer, String>();
-            while ((line = kernelReader.readLine()) != null) {
-                this.kernelMap.put(kernelFreq, line);
-                kernelFreq++;
-            }
-        } finally {
-            kernelReader.close();
-        }
-    }
+	public void loadKernel(Path cachePath) throws IOException {
+		BufferedReader kernelReader = new BufferedReader(new FileReader(cachePath.toString()));
+		try {
+			String line = "";
+			int kernelFreq = KERNEL_START_FREQ;
+			this.kernelMap = new HashMap<Integer, String>();
+			while ((line = kernelReader.readLine()) != null) {
+				this.kernelMap.put(kernelFreq, line);
+				kernelFreq++;
+			}
+		} finally {
+			kernelReader.close();
+		}
+	}
 
-    public short[] ConvertStringArrayToShortArray(String[] stringArray){
-        short shortArray[] = new short[stringArray.length];
+	public short[] ConvertStringArrayToShortArray(String[] stringArray){
+		short shortArray[] = new short[stringArray.length];
 
-        for(int i = 0; i < stringArray.length; i++){
-            shortArray[i] = Short.parseShort(stringArray[i]);
-        }
+		for(int i = 0; i < stringArray.length; i++){
+			shortArray[i] = Short.parseShort(stringArray[i]);
+		}
 
-        return shortArray;
-    }
-    
+		return shortArray;
+	}
+	
 	public void map(LongWritable inkey, Text value,
-        OutputCollector<NullWritable, Text> output,
-        Reporter reporter) throws IOException {
+		OutputCollector<NullWritable, Text> output,
+		Reporter reporter) throws IOException {
 
-        rec = RChannelDataPoint.parse(value.toString());
+		rec = RChannelDataPoint.parse(value.toString());
 
-        try {
-            
-            if (lastTimestamp > rec.getTimestamp()) {
-                throw new IOException("Timestamp not sorted at: " + lastTimestamp + " and " + rec.getTimestamp());
-            }
-            
-            lastTimestamp = rec.getTimestamp();
-            
-            if ( n == SIGNAL_BUFFER_SIZE ) {
-                n = 0;
-                for (int j = SIGNAL_BUFFER_SIZE-KERNEL_WINDOW_SIZE+1; j<SIGNAL_BUFFER_SIZE; j++) {
-                   signal[n] = signal[j];            
-                   n++;
-                } //for
-            } // if
-        
-            signal[n] = rec.getVoltage();
+		try {
+			
+			if (lastTimestamp > rec.getTimestamp()) {
+				throw new IOException("Timestamp not sorted at: " + 
+					lastTimestamp + " and " + 
+					rec.getTimestamp()
+					);
+			}
+			
+			lastTimestamp = rec.getTimestamp();
+			
+			if ( n == SIGNAL_BUFFER_SIZE ) {
+				n = 0;
+				for (int j = SIGNAL_BUFFER_SIZE-KERNEL_WINDOW_SIZE+1; j<SIGNAL_BUFFER_SIZE; j++) {
+				   signal[n] = signal[j];            
+				   n++;
+				} //for
+			} // if
+		
+			signal[n] = rec.getVoltage();
 
-            if (n>=KERNEL_WINDOW_SIZE-1) {
+			if (n>=KERNEL_WINDOW_SIZE-1) {
 
-                for (int k = KERNEL_START_FREQ; k <= KERNEL_END_FREQ; k++) {
-                    ckConvolution[k] = 0;
-                    
-                    for (int i = n-KERNEL_WINDOW_SIZE+1, j=0; i <= n && j < KERNEL_WINDOW_SIZE; i++, j++) {
-                        ckConvolution[k] += signal[i]*kernelStack[k][j];
-                    } // for
+				for (int k = KERNEL_START_FREQ; k <= KERNEL_END_FREQ; k++) {
+					ckConvolution[k] = 0;
+					
+					for (int i = n-KERNEL_WINDOW_SIZE+1, j=0; i <= n && j < KERNEL_WINDOW_SIZE; i++, j++) {
+						ckConvolution[k] += signal[i]*kernelStack[k][j];
+					} // for
 
-                    out_value.set(ratnumber + "," + 
-                                sessiondate + "," +
-					    		lastTimestamp + "," +
-								channelid + "," +
-								k + "," +
-								ckConvolution[k]);
-                    output.collect(NullWritable.get(), out_value);
-                    
-                } //for
-            } // if
+					out_value.set(
+						lastTimestamp + "," +
+						k + "," +
+						ckConvolution[k])
+					;
+					output.collect(NullWritable.get(), out_value);
+					
+				} //for
+			} // if
 
-            n++;
+			n++;
 
-        } catch (IOException ioe) {
-            System.err.println(ioe.getMessage());
-            System.exit(0);
-       }
+		} catch (IOException ioe) {
+			System.err.println(ioe.getMessage());
+			System.exit(0);
+	   }
 	} // map
 
 }
